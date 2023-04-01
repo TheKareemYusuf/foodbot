315a146a90731a20f5e9b52446c0b19d168039f0
Yusuf <yakande96@gmail.com>
2023-04-01 12:00:48 +0100

    implemented basic functionality

--- app.js
+++ app.js
@@ -5,37 +5,41 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 const server = createServer(app);
 // const io = new Server(server);
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
 const CONFIG = require("./config/config");
 
+// Connecting to MongoDB
 const { connetToMongoDB } = require("./db");
 connetToMongoDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Creating the session store
 const sessionStore = new MongoStore({
   mongoUrl: CONFIG.DATABASE_URL,
-  collectionName: 'sessions'
-})
+  collectionName: "sessions",
+});
 
+// Creating the session middleware
 const sessionMiddleWare = session({
   secret: CONFIG.SECRET_KEY,
   resave: true,
   saveUninitialized: true,
   store: sessionStore,
   cookie: {
-    maxAge: 1000 * 60 * 60 * 24
-  }
-})
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+});
 
 app.use(sessionMiddleWare);
 
 app.use(express.static("public"));
 
 
+// Creating a request object so we can have access to "req"
 const io = new Server(server, {
   allowRequest: (req, callback) => {
     // with HTTP long-polling, we have access to the HTTP response here, but this is not
@@ -68,12 +72,8 @@ io.engine.on("initial_headers", (headers, req) => {
   }
 });
 
-const menuOptions = [
-  { id: 1, command: "11", name: "Rice, beans and dodo", price: 2000 },
-  { id: 2, command: "12", name: "Bread, beans and dodo", price: 2000 },
-  { id: 3, command: "13", name: "Amala, ewedu and goat meat", price: 3000 },
-];
- 
+// Requiring the menu options
+const menuOptions = require('./models/menuModel');
 
 const pattern = /^1[1-9]$/;
 io.on("connection", (socket) => {
@@ -83,39 +83,69 @@ io.on("connection", (socket) => {
   socket.join(req.session.id);
   console.log(req.session.id);
 
-
-
   socket.on("chat message", (userMessage) => {
     console.log("message: " + userMessage);
-    let botResponse;
-    // console.log(req.session);
-    if (userMessage === "1") {
-      botResponse = `Please select an option from the menu:\n `;
-      menuOptions.forEach((option) => {
-        botResponse += `Select ${option.command}: ${option.name} - ${option.price} Naira \n`;
-      });
-    } else if (pattern.test(userMessage)){
-      // userMessage = selectedOption
-      const selectedOption = menuOptions.find(option => option.command === userMessage);
-      if (selectedOption) {
-        botResponse = `You selected ${selectedOption.name} for ${selectedOption.price} Naira. Please type 99 to checkout your order.`;
-        
-      } else {
-        botResponse = 'Your option is not available, press 1 to place your order'
+    req.session.reload((err) => {
+      let botResponse;
+      if (err) {
+        // session has expired
+        return socket.disconnect();
       }
+      let orderHistory = req.session.orderHistory;
+      let menuChoice = req.session.menuChoice;
+      if (userMessage === "1") {
+        botResponse = `Please select an option from the menu:\n `;
+        menuOptions.forEach((option) => {
+          botResponse += `Select ${option.command}: ${option.name} - N${option.price}, \n`;
+        });
+      } else if (pattern.test(userMessage)) {
+        // userMessage = selectedOption
+        const selectedOption = menuOptions.find(
+          (option) => option.command === userMessage
+        );
+        if (selectedOption) {
+          req.session.menuChoice = selectedOption;
+          req.session.save(() => {
+            req.session.menuChoice;
+          });
+          botResponse = `You selected ${selectedOption.name} for N${selectedOption.price}. Please type 99 to checkout your order.`;
+        } else {
+          botResponse =
+            "Your option is not available, press 1 to place your order";
+        }
       } else if (userMessage === "97") {
-      botResponse = "Here is your current order";
-    } else if (userMessage === "98") {
-      botResponse = "Here is your order history";
-    } else if (userMessage === "99") {
-      botResponse = "Thank you for shopping with us!";
-    } else if (userMessage === "0") {
-      botResponse = "Your order has been cancelled";
-    } else {
-      botResponse = "I did not understand your message. Please try again.";
-    }
-
-    io.emit("chat message", botResponse);
+        if (menuChoice) {
+          botResponse = `Here is your current order: Name: ${menuChoice.name}  Price: N${menuChoice.price}`;
+        }
+      } else if (userMessage === "98") {
+        // req.session.orderHistory = [];
+        if (orderHistory) {
+          botResponse = `Here is your order history:\n `;
+          orderHistory.forEach((option) => {
+            botResponse += `${option.name} - N${option.price} \n`;
+          });
+        }
+      } else if (userMessage === "99") {
+        if (menuChoice) {
+          orderHistory.push(menuChoice);
+          req.session.save(() => {
+            orderHistory;
+          });
+          botResponse = "Your food is on its way! Thank you for your patronage.";
+        }
+      } else if (userMessage === "0") {
+        if (menuChoice) {
+          cancelledOrder = orderHistory.pop()
+          req.session.save(() => {
+            orderHistory;
+          });
+          botResponse = `Your order to buy ${cancelledOrder.name} has been cancelled`;
+        }
+      } else {
+        botResponse = "I did not understand your message. Please try again.";
+      }
+      io.emit("chat message", botResponse);
+    });
   });
 
   socket.on("disconnect", () => {
--- models/menuModel.js
+++ models/menuModel.js
@@ -20,3 +20,12 @@ const menuSchema = new mongoose.Schema({
 const Menu = mongoose.model("Menu", menuSchema);
 
 module.exports = Menu;
+const menuOptions = [
+  { id: 1, command: "11", name: "Rice, Beans and Dodo", price: 2000 },
+  { id: 2, command: "12", name: "Bread, Beans and Dodo", price: 2000 },
+  { id: 3, command: "13", name: "Amala, Ewedu with Goat Meat", price: 3000 },
+  { id: 4, command: "14", name: "Semo and Egusi with Fish", price: 3000 },
+];
+
+
+module.exports = menuOptions
